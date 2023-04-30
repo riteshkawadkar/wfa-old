eab8092adc838eda0dd8e5e1728758ee58782aae
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-04-30 17:38:11 +0530

    git commit -m "Removed unnecessory pages"

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ $controller_path = 'App\Http\Controllers';
 Route::get('/', $controller_path . '\pages\HomePage@index')->name('pages-home');
 Route::get('/page-2', $controller_path . '\pages\Page2@index')->name('pages-page-2');
 
-// pages
-Route::get('/pages/misc-error', $controller_path . '\pages\MiscError@index')->name('pages-misc-error');
+//// pages
+//Route::get('/pages/misc-error', $controller_path . '\pages\MiscError@index')->name('pages-misc-error');
 
-// authentication
-Route::get('/auth/login-basic', $controller_path . '\authentications\LoginBasic@index')->name('auth-login-basic');
-Route::get('/auth/register-basic', $controller_path . '\authentications\RegisterBasic@index')->name('auth-register-basic');
+//// authentication
+//Route::get('/auth/login-basic', $controller_path . '\authentications\LoginBasic@index')->name('auth-login-basic');
+//Route::get('/auth/register-basic', $controller_path . '\authentications\RegisterBasic@index')->name('auth-register-basic');
 
 Route::middleware([
     'auth:sanctum',
