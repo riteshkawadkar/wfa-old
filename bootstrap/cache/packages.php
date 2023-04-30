070bcf73cccc83c8864bc5db59d7dfaa91009563
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-04-30 16:49:45 +0530

    git commit -m "Added ACL - Spatie Permissions"

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -95,6 +95,13 @@
       'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
     ),
   ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
   'themeselection/sneat-html-laravel-jetstream' => 
   array (
     'providers' => 
